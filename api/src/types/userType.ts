bf9a2693754f097f4545a37a4e009268140b4b71
Mert KARTAL <179147337+Mert-Kartal@users.noreply.github.com>
2025-06-03 06:04:28 +0300

    refactored authValidation and authType to userType and userValidation. added updateUser validation,type and changePassword validation

--- api/src/types/userType.ts
+++ api/src/types/userType.ts
@@ -1,8 +1,10 @@
 import { z } from "zod";
 import { signUpSchema } from "../validations/authValidation";
+import { updateUserSchema } from "../validations/userValidation";
 
-// Database model type without verifyPassword
 export type CreateUserData = Omit<
   z.infer<typeof signUpSchema>,
   "verifyPassword"
 >;
+
+export type UpdateUserData = z.infer<typeof updateUserSchema>;
